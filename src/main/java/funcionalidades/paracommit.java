987b3306c947a28367d176d103a8139f17f943d0
JosueEmilian <juyul@miumg.edu.gt>
2022-08-28 20:55:23 -0600

    prueba

---
+++ src/main/java/funcionalidades/paracommit.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package funcionalidades;
+
+/**
+ *
+ * @author josueemilian
+ */
+public class paracommit {
+    
+    //simplemente vamos a probra si se guarda xD
+    
+}
